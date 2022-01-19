7b2685c928a2b330d012d6abaf8d572da86466ad
Ado Dragicevic <adodragicevic@gmail.com>
2022-01-19 20:08:01 +0100

    Add useState and useEffect for calculating columnsWidths in TTable component. This way the widts are not changed if the table row with the longest string is deleted

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TableCtx } from "../contexts/table";
 import { TableProps } from "../models/props";
 import { getLocationFromId, getTable, getTableName, getColumnsWidths, getTableRows } from "../helpers/table";
@@ -8,17 +8,23 @@ import TableHeader from "./TableHeader";
 
 const Table = ({ id = "" }: TableProps) => {
 
-  console.log("Table", id);
-
   const data = useContext(TableCtx);
-
+  
+  const [columnsWidths, setColumnWidths] = useState("");
+  
+  const location = getLocationFromId(id),
+        table    = getTable(data, location);
+  
+  useEffect( () => {
+    const columnsWidths = getColumnsWidths(table).join("__$__");
+    setColumnWidths(columnsWidths);
+  }, [] ); // I intentionally do not want to recalcuate column widths each time table row is deleted
+  
   if (!data.length) return <EmptyTable />;
-  const location = getLocationFromId(id);
-  const table = getTable(data, location);
-  const name = getTableName(location) as string;
-  const labels = Object.keys(table[0].data).join("__$__");
-  const columnsWidths = getColumnsWidths(table).join("__$__");
-  const tableRows = getTableRows(table, id, columnsWidths);
+  
+  const name      = getTableName(location) as string,
+        labels    = Object.keys(table[0].data).join("__$__"),
+        tableRows = getTableRows(table, id, columnsWidths);
 
   return (
     <div className="table">
@@ -30,6 +36,13 @@ const Table = ({ id = "" }: TableProps) => {
 
 export default Table;
 
+
+// Why I'm not using useCallback?
 // In this case, I think turing arr into str (with labels and columnWidths) is more optimal than 
 // useCallback. Reasoning that table will always be changed in setState. Even if we have one table 
-// and we delete one row we will use table.filter, which would change the useCallback dependancy arr, i.e. [table]
+// and we delete one row we will use table.filter, which would affect useCallback dependancy arr, i.e. [table]
+
+// Why I'm using useEffect?
+// Column widths are calculated dynamically, i.e. based on longest string in any table row. If one row
+// has a long name, and that row is deleted, then column widths are recalucated again and that columns width
+// decreases. I found this irritating from UX perspective, and applied useEffect with empty dependancy arr.
