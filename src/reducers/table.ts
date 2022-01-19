0237d4493e8a3aa2c3f1b786e1e08dfa06dda2cf
Ado Dragicevic <adodragicevic@gmail.com>
2022-01-19 18:51:01 +0100

    Delete previous reducer delete logic

--- src/reducers/table.ts
+++ src/reducers/table.ts
@@ -3,10 +3,14 @@ import { deleteTableRow, getLocationFromId } from "../helpers/table";
 
 
 const tableReducer: TableReducer = (state, action) => {
+  
   switch(action.type) {
+    
     case(TableDispatchActionType.DELETE):
+      
       const location = getLocationFromId(action.id);
       return deleteTableRow(state, location); 
+    
     default:
       return state;
   }
@@ -14,23 +18,3 @@ const tableReducer: TableReducer = (state, action) => {
 }
 
 export default tableReducer;
-
-
-
-
-// Previous delete solution:
-
-/*
-      const copy     = JSON.parse(JSON.stringify(state)),
-            location = getLocationFromId(action.id),
-            rowIndx  = location.pop() as number,
-            table    = getTable(copy, location);
-      table.splice(rowIndx, 1);
-      if (!table.length) { // if table has no more rows, delete whole table
-        const tableName   = location.pop() as string,
-              row         = location.pop() as number,
-              parentTable = getTable(copy, location);
-        if (parentTable[row]) delete parentTable[row].kids[tableName];
-      }
-      return copy;
-      */
